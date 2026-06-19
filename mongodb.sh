457c1dfb97090898fd1af0b172a5ad561b2331fd
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 18:54:58 +0530

    mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -32,3 +32,7 @@ VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>> $LOGS_FILE
 VALIDATE $? "Installing MongoDB"
+
+systemctl enable --now mongod
+
+VALIDATE $? "Starting and Enabling mongod"
