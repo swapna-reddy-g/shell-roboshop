00b0cfccdebd332f8fbecbf8f711a0cf9a45c014
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 21:09:13 +0530

    mongodb.sh

---
+++ roboshop.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# To create instance through CLI:
+# aws ec2 run instances \
+#     --image-id ami-0220d79f3f480ecf5 \
+#     --instace-type t3.micro \
+#     --security-groups roboshop-common \
+#         --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}] \
+#         --query "Instance[0].InstanceId' \
+#     --output text
+
+# To get details about instance
+# aws ec2 describe-instances --instance-ids i-0aed2fbad8d2d74af --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z02881716WCB6UDT9VO7"
+DOMAIN_NAME="swadevops.online"
+
+for instance in $@
+do
+    echo "Launching Instance: roboshop-$instance"
+    INSTANCE_ID=(aws ec2 run instances \
+    --image-id ami-0220d79f3f480ecf5 \
+    --instace-type t3.micro \
+    --security-groups "shell-scripting" "roboshop-$instance" \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="roboshop-$instance"}] \
+    --query "Instance[0].InstanceId' \
+    --output text
+    )
+    echo "Instance ID: $INSTANCE_ID"
+
+    if [ $instance == "Frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids i-0aed2fbad8d2d74af \
+        --query 'Reservations[*].Instances[*].PublicIpAddress' \
+        --output text
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids i-0aed2fbad8d2d74af \
+        --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+        --output text)
+        R53_RECORD="$instance.$DOMAIN_NAME"
+    fi
+
+    # Updating Route 53 Record:
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+        {
+            "Comment": "Update A record to new IP" \
+            "ResourceRecordSet": {
+                "Name": "$ROUTE53_RECORD"
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                    {
+                        "Value": "$IP"
+                    }
+                ]
+            }
+        }
+    '
+done
