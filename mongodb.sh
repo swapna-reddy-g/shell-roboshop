7bed51374a822c485f0b9ed3ba82b42910bb98ef
swapna-reddy-g <swapnareddychinni12@gmail.com>
2026-06-19 19:09:08 +0530

    mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -36,7 +36,7 @@ VALIDATE $? "Installing MongoDB"
 systemctl enable --now mongod
 VALIDATE $? "Starting and Enabling mongod"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote Connections to MongoDB"
 
 systemctl restart mongod
